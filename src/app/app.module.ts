a9084d57d68ec4139ccb976baa0e0ccfde8001fb
Daniel Silva <daniel.silva@qualityautomacao.com>
2022-08-12 14:00:18 -0300

    RESTFULL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MesesComponent } from './meses/meses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MesesComponent
   ],
   imports: [
     BrowserModule
